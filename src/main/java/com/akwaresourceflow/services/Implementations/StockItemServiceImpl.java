e0328b489d3fe7a27a0f481223894e990a526b8b
HACHIM <hachim.youssef10@gmail.com>
2024-07-04 13:32:46 +0200

    Fixed Stock item Service and stock item implemntation

---
+++ src/main/java/com/akwaresourceflow/services/Implementations/StockItemServiceImpl.java
@@ -0,0 +1,50 @@
+package com.akwaresourceflow.services.Implementations;
+
+import com.akwaresourceflow.models.StockItem;
+import com.akwaresourceflow.repositories.StockItemRepo;
+import com.akwaresourceflow.services.Interfaces.StockItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StockItemServiceImpl implements StockItemService {
+    private final StockItemRepo stockItemRepo;
+
+    @Autowired
+    public StockItemServiceImpl(StockItemRepo stockItemRepo) {
+        this.stockItemRepo = stockItemRepo;
+    }
+
+    @Override
+    public List<StockItem> getAllStockItem() {
+        return stockItemRepo.findAll();
+    }
+
+    @Override
+    public void deleteStockItem(Long idStockItem) {
+        Optional<StockItem> stockItemOptional = stockItemRepo.findById(idStockItem);
+        if (stockItemOptional.isPresent()) {
+            stockItemRepo.delete(stockItemOptional.get());
+        } else {
+            throw new RuntimeException("StockItem not found with id: " + idStockItem);
+        }
+    }
+
+    @Override
+    public void updateStockItem(Long idStockItem, StockItem stockItem) {
+        Optional<StockItem> stockItemOptional = stockItemRepo.findById(idStockItem);
+        if (stockItemOptional.isPresent()) {
+            StockItem existingStockItem = stockItemOptional.get();
+            existingStockItem.setName(stockItem.getName());
+            existingStockItem.setQuantity(stockItem.getQuantity());
+            existingStockItem.setThreshold(stockItem.getThreshold());
+            stockItemRepo.save(existingStockItem);
+        } else {
+            throw new RuntimeException("StockItem not found with id: " + idStockItem);
+        }
+
+    }
+}
--- src/main/java/com/akwaresourceflow/services/Interfaces/StockItemService.java
+++ src/main/java/com/akwaresourceflow/services/Interfaces/StockItemService.java
@@ -1,4 +1,11 @@
 package com.akwaresourceflow.services.Interfaces;
 
+import com.akwaresourceflow.models.StockItem;
+
+import java.util.List;
+
 public interface StockItemService {
+    List<StockItem> getAllStockItem();
+    void deleteStockItem(Long idStockItem);
+    void updateStockItem(Long idStockItem,StockItem stockItem);
 }
