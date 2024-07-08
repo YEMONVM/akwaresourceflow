23f95cd941da356e4d1e7564ce4d5d9a63e7a77c
YEMONVM <yemonvm@gmail.com>
2024-07-08 11:11:01 +0100

    @CrossOrigin in controllers

--- src/main/java/com/akwaresourceflow/controllers/VehicleController.java
+++ src/main/java/com/akwaresourceflow/controllers/VehicleController.java
@@ -10,6 +10,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/vehicles")
 public class VehicleController {
 
