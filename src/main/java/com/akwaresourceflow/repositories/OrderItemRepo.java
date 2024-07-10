a371790c9b7288b17fce13555e18ac47de191ab8
HACHIM <hachim.youssef10@gmail.com>
2024-07-10 13:43:37 +0200

    add order item repo

--- src/main/java/com/akwaresourceflow/repositories/OrderItemRepo.java
+++ src/main/java/com/akwaresourceflow/repositories/OrderItemRepo.java
@@ -2,8 +2,14 @@ package com.akwaresourceflow.repositories;
 
 import com.akwaresourceflow.models.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderItemRepo extends JpaRepository<OrderItem, Long> {
+
+    @Query("SELECT o.productName, SUM(o.quantity) as totalQuantity FROM OrderItem o GROUP BY o.productName ORDER BY totalQuantity DESC")
+    List<Object[]> findTopSellingProducts();
 }
